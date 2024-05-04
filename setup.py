349788279d3289a47154d28e04b0019312443f6b
elprofesor <rrazvan.radeanu@gmail.com>
2024-05-04 18:39:17 +0300

    changed version in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name="operation.repo",
-    version="2.0.0",
+    version="2.1.0",
     description="operation.repo",
     author="elprofesor96",
     py_modules=["op", "ConfigHandler", "OpClass", "OpClassToServer"],
